3bf6cb04fe2b2fbd1ae9c19654d3fbb327e54a33
Alan M. Turing <1912AlanTuring1954@gmail.com>
2023-07-25 13:50:02 -0700

    first commit

---
+++ jarvis.py
@@ -0,0 +1,93 @@
+import random
+import datetime
+import json
+import torch
+from brain.model import NeuralNet
+from brain.nltk_utils import bag_of_words, tokenize
+from TTS_.tts import text_to_speech
+from functions.opinion import opinion
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open('intents.json', 'r') as json_data:
+    intents = json.load(json_data)
+
+FILE = "data/data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+all_words = data['all_words']
+tags = data['tags']
+model_state = data["model_state"]
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+def get_time():
+    return datetime.datetime.now().time().strftime('%I:%M %p')
+
+prev_tag = ""
+prev_input = ""
+
+while True:
+    wake_up = input("jarvis sleeping: ")
+
+    if 'jarvis' == wake_up.lower():
+
+        while True:
+            user_input = input("jarvis awake: ")
+            
+            if user_input.lower() == prev_input.lower():
+                tag = "repeat_string"
+            
+            elif prev_tag == 'technical':
+                pass
+                
+            else:
+                sentence = tokenize(user_input)
+                X = bag_of_words(sentence, all_words)
+                X = X.reshape(1, X.shape[0])
+                X = torch.from_numpy(X).to(device)
+                output = model(X)
+                _, predicted = torch.max(output, dim=1)
+                tag = tags[predicted.item()]
+                probs = torch.softmax(output, dim=1)
+                prob = probs[0][predicted.item()]
+
+            if prob.item() > 0.785:
+                for intent in intents['intents']:
+                    if tag == intent["tag"]:
+                        
+                        if intent["tag"] == "repeat_string":
+                            response = random.choice(intent["responses"])
+                            text_to_speech(response.replace("sir", "Mr Stark"))
+                            print(intent["tag"])
+                            
+                        elif intent['tag'] == 'opinion':
+                            text_to_speech(opinion(user_input))
+                            prev_tag = intent['tag']
+                        
+                        elif intent["tag"] == "time":
+                            res_time = random.choice(intent['responses']).replace("{time}", get_time())
+                            text_to_speech(f"{res_time}")
+                            print(intent['tag'])
+                            prev_tag = intent['tag']
+                        
+                        else:
+                            text_to_speech(f"{random.choice(intent['responses'])}")
+                            print(intent['tag'])
+                            prev_tag = intent['tag']
+
+                prev_input = user_input.lower()
+                
+            else:
+                for intent in intents['intents']:
+                    if intent["tag"] == 'technical':
+                        text_to_speech(f"{random.choice(intent['responses'])}")
+                        print(intent['tag'])
+                        
+    else:
+        pass
---
+++ training/train_model.py
@@ -0,0 +1,63 @@
+import datasets
+import torch
+from transformers import AutoTokenizer, AutoModelForMaskedLM, Trainer, TrainingArguments, DataCollatorForLanguageModeling
+
+def main():
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        print("GPU available.")
+        print(torch.cuda.get_device_name(0))  # Prints the name of the first GPU
+    else:
+        device = torch.device("cpu")
+        print("GPU not available. Using CPU.")
+
+    # Step 1: Load the 'wikipedia' dataset with required columns
+    dataset = datasets.load_dataset("wikipedia", "20220301.en", split="train")
+
+    # Step 2: Keep only the 'id', 'title', and 'text' columns in the dataset
+    dataset.set_format(type='torch', columns=['id', 'title', 'text'])
+
+    # Step 3: Preprocess the data
+    tokenizer = AutoTokenizer.from_pretrained("roberta-base")
+
+    def tokenize_function(example):
+        return tokenizer(example["text"], padding="max_length", truncation=True)
+
+    # Step 4: Tokenize the filtered dataset to add the required columns
+    tokenized_dataset = dataset.map(tokenize_function, batched=True)
+
+    # Step 5: Create the model for masked language modeling (MLM)
+    model = AutoModelForMaskedLM.from_pretrained("roberta-base")
+
+    training_args = TrainingArguments(
+        per_device_train_batch_size=32,
+        num_train_epochs=3,
+        output_dir="model",
+        logging_dir="logs",
+        logging_steps=100,
+    )
+
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer, 
+        mlm_probability=0.15,
+        padding=True,
+        max_length=128,  # Set your desired maximum length for the model input
+        return_tensors="pt"  # Return PyTorch tensors
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_dataset,  # Use the tokenized dataset directly
+        data_collator=data_collator,
+        device=device,  # Specify the device to use (GPU or CPU)
+    )
+
+    # Step 6: Train the model on the masked text
+    trainer.train()
+
+    # Step 7: Save the trained model
+    model.save_pretrained("model_jarvis")
+
+if __name__ == "__main__":
+    main()
