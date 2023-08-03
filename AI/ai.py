ae359e890a58e6cf9f82216e2ca085fccc451fb4
Alan M. Turing <1912AlanTuring1954@gmail.com>
2023-08-02 19:39:20 -0700

    AI

--- AI/ai.py
+++ AI/ai.py
@@ -1,4 +1,3 @@
-import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModelForSequenceClassification
 from langchain.embeddings import HuggingFaceEmbeddings
 import tiktoken
@@ -6,17 +5,17 @@ import tiktoken
 def initialize_chatbot():
     # Load GPT-3-based chatbot model and tokenizer
     chatbot_model_name = 'h2oai/h2ogpt-oasst1-512-12b'
-    chatbot_model = AutoModelForCausalLM.from_pretrained(chatbot_model_name).to('cuda')
+    chatbot_model = AutoModelForCausalLM.from_pretrained(chatbot_model_name)
     chatbot_tokenizer = AutoTokenizer.from_pretrained(chatbot_model_name)
 
     # Load reward model and tokenizer
     reward_model_name = 'OpenAssistant/reward-model-deberta-v3-large-v2'
-    reward_model = AutoModelForSequenceClassification.from_pretrained(reward_model_name).to('cuda')
+    reward_model = AutoModelForSequenceClassification.from_pretrained(reward_model_name)
     reward_tokenizer = AutoTokenizer.from_pretrained(reward_model_name)
 
     # Load sentence embedding model
     embedding_model_name = "sentence-transformers/all-MiniLM-L6-v2"
-    embedding_model = HuggingFaceEmbeddings(model_name=embedding_model_name, model_kwargs='cuda')
+    embedding_model = HuggingFaceEmbeddings(model_name=embedding_model_name, model_kwargs='cpu')
 
     # Configure tokenization settings (if needed)
     encoding = tiktoken.get_encoding("cl100k_base")
@@ -26,7 +25,7 @@ def initialize_chatbot():
 
 def generate_response(chatbot_model, chatbot_tokenizer, user_input):
     # Generate chatbot response using the chatbot model and tokenizer
-    inputs = chatbot_tokenizer(user_input, return_tensors="pt").to('cuda')
+    inputs = chatbot_tokenizer(user_input, return_tensors="pt")
     outputs = chatbot_model.generate(**inputs)
     response = chatbot_tokenizer.decode(outputs[0], skip_special_tokens=True)
 
@@ -34,7 +33,7 @@ def generate_response(chatbot_model, chatbot_tokenizer, user_input):
 
 def calculate_reward(reward_model, reward_tokenizer, response):
     # Calculate reward for the generated response using the reward model and tokenizer
-    inputs = reward_tokenizer(response, return_tensors="pt").to('cuda')
+    inputs = reward_tokenizer(response, return_tensors="pt")
     outputs = reward_model(**inputs)
     reward = outputs.logits.softmax(dim=1)[0][1].item()  # Assuming class 1 is the positive reward class
 
