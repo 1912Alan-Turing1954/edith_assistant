b025cc6e0a47f4e7adbd7bf6163052f31b95406c
Alan M. Turing <1912AlanTuring1954@gmail.com>
2023-08-18 18:04:29 -0700

    commit

--- Friday_testing.py
+++ Friday_testing.py
@@ -3,22 +3,22 @@ import random
 import datetime
 import json
 import torch
+from word2number import w2n
 import concurrent.futures
 from brain.model import NeuralNet
 from brain.nltk_utils import bag_of_words, tokenize
 from tts_.tts import text_to_speech
-from functions.opinion import opinion
 from AI.flan_t5_large_model import generative_with_t5
-from functions._math import solve_word_math_expression
-from functions.time_of_day import time_of_day_correct
-from functions.three_math_sim import create_simlulation_function
-from functions.location import (
+from functions.math._math import solve_word_math_expression
+from functions.math.three_math_sim import create_simlulation_function
+from functions.system.time_of_day import time_of_day_correct
+from functions.system.location import (
     get_address_description,
     get_location_description,
     get_long_and_lati,
 )
 
-from functions.system_info import (
+from functions.system.system_info import (
     get_system_info,
     generate_system_status_response,
     generate_storage_status_response,
@@ -52,9 +52,67 @@ class Friday:
         self.prev_response = ""
         self.mute = False
 
-    def is_complex_alphabetical_math_problem(self, user_input):
-        alphabetic_math_pattern = r"(?i)\b(?:what is the|evaluate the)?\s*(?:sum of|difference between|product of|square of|cube of)?\s*(?:zero|one|two|three|four|five|six|seven|eight|nine|ten)\b\s*(?:plus|minus|times|multiplied by|divided by|\+|\-|\*|\/|\^|and)\s*\b(?:zero|one|two|three|four|five|six|seven|eight|nine|ten)\b"
-        return bool(re.search(alphabetic_math_pattern, user_input))
+    # def is_complex_alphabetical_math_problem(self, user_input):
+    #     alphabetic_math_pattern = r"(?i)\b(?:what is the|evaluate the)?\s*(?:sum of|difference between|product of|square of|cube of)?\s*(?:zero|one|two|three|four|five|six|seven|eight|nine|ten)\b\s*(?:plus|minus|times|multiplied by|divided by|\+|\-|\*|\/|\^|and)\s*\b(?:zero|one|two|three|four|five|six|seven|eight|nine|ten)\b"
+    #     return bool(re.search(alphabetic_math_pattern, user_input))
+
+    def convert_textual_numbers(self, user_input):
+        words = user_input.lower().split()  # Split input into words
+        numerical_words = []
+
+        for word in words:
+            try:
+                num = w2n.word_to_num(word)
+                numerical_words.append(str(num))
+            except ValueError:
+                numerical_words.append(word)  # Keep non-convertible words as they are
+
+        processed_input = " ".join(numerical_words)
+        return processed_input.strip()
+
+    def extract_function_from_input(self, user_input):
+        for intent in self.intents["intents"]:
+            if intent["tag"] == "simulate_interference":
+                for pattern in intent["patterns"]:
+                    if pattern.lower() in user_input:
+                        user_input = user_input.replace(pattern, "")
+                    else:
+                        user_input = user_input.lower()
+
+        replacements = {
+            "cosine of x": "cos(x)",
+            "sine of x": "sin(x)",
+            "tangent of x": "tan(x)",
+            "square root of x": "sqrt(x)",
+            "logarithm base 10 of x": "log10(x)",
+            "natural logarithm of x": "log(x)",
+            "exponential of x": "exp(x)",
+            "absolute value of x": "abs(x)",
+            "cosine of y": "cos(y)",
+            "sine of y": "sin(y)",
+            "tangent of y": "tan(y)",
+            "square root of y": "sqrt(y)",
+            "logarithm base 10 of y": "log10(y)",
+            "natural logarithm of y": "log(y)",
+            "exponential of y": "exp(y)",
+            "absolute value of y": "abs(y)",
+            "x squared": "x**2",
+            "x cubed": "x**3",
+            "y squared": "y**2",
+            "y cubed": "y**3",
+            "equals": "==",
+            "equal to": "==",
+            "plus": "+",
+            "minus": "-",
+            "times": "*",
+            "divided by": "/",
+            "greater than": ">",
+            "less than": "<",
+            "not equal to": "!=",
+        }
+        for keyword, replacement in replacements.items():
+            user_input = user_input.replace(keyword, replacement)
+        return user_input.strip()
 
     def get_tag_from_response(self, response):
         for intent in self.intents["intents"]:
@@ -128,19 +186,20 @@ class Friday:
                 tag = self.tags[predicted.item()]
                 probs = torch.softmax(output, dim=1)
                 prob = probs[0][predicted.item()]
-            if self.is_complex_alphabetical_math_problem(user_input):
-                result = solve_word_math_expression(user_input)
-                for intent in self.intents["intents"]:
-                    if intent["tag"] == "math_tsk":
-                        response = random.choice(intent["responses"]).format(
-                            answer=result
-                        )
-                        text_to_speech(response)
-                        print(intent["tag"])
-                        print(response)
-                        break
 
-            elif prob.item() > 0.95:
+            # if self.is_complex_alphabetical_math_problem(user_input):
+            #     result = solve_word_math_expression(user_input)
+            #     for intent in self.intents["intents"]:
+            #         if intent["tag"] == "math_tsk":
+            #             response = random.choice(intent["responses"]).format(
+            #                 answer=result
+            #             )
+            #             text_to_speech(response)
+            #             print(intent["tag"])
+            #             print(response)
+            #             break
+
+            if prob.item() > 0.95:
                 for intent in self.intents["intents"]:
                     if tag == intent["tag"]:
                         if intent["tag"] == "background_acknowledgment":
@@ -148,7 +207,7 @@ class Friday:
 
                         elif intent["tag"] == "mute_command":
                             self.mute = True
-                            pass
+                            continue
 
                         elif intent["tag"] == "location_inquiry":
                             response = random.choice(intent["responses"])
@@ -166,6 +225,8 @@ class Friday:
                             text_to_speech(response)
 
                         elif intent["tag"] == "simulate_interference":
+                            user_input = self.convert_textual_numbers(user_input)
+                            user_input = self.extract_function_from_input(user_input)
                             response = random.choice(intent["responses"])
                             text_to_speech(response)
                             with concurrent.futures.ThreadPoolExecutor(
@@ -173,7 +234,7 @@ class Friday:
                             ) as executor:
                                 # Run input processing and function conversion in parallel
                                 future = executor.submit(
-                                    create_simlulation_function, response
+                                    create_simlulation_function, user_input
                                 )
                                 # Wait for the future to complete
                                 future.result()
@@ -181,41 +242,49 @@ class Friday:
                         elif intent["tag"] == "repeat_tsk":
                             response = random.choice(intent["responses"])
                             text_to_speech(f"{response} {self.prev_response}")
+
                         elif intent["tag"] == "repeat_string":
                             response = random.choice(intent["responses"])
                             self.get_intent_response(intent, response)
+
                         elif intent["tag"] == "good_morning":
                             response = time_of_day_correct(user_input)
                             self.get_intent_response(intent, response)
+
                         elif intent["tag"] == "good_afternoon":
                             response = time_of_day_correct(user_input)
                             self.get_intent_response(intent, response)
+
                         elif intent["tag"] == "good_night":
                             response = time_of_day_correct(user_input)
                             self.get_intent_response(intent, response)
+
                         elif intent["tag"] == "generative_with_t5":
                             response = random.choice(intent["responses"]).format(
                                 string=generative_with_t5(user_input)
                             )
                             self.get_intent_response(intent, response)
+
                         elif intent["tag"] == "system_info_tsk":
                             response = random.choice(intent["responses"])
                             self.get_intent_response(intent, response, system_info)
+
                         elif intent["tag"] == "storage_info_tsk":
                             response = random.choice(intent["responses"])
                             self.get_intent_response(intent, response, storage_info)
+
                         elif intent["tag"] == "cpu_usage_tsk":
                             response = random.choice(intent["responses"])
                             self.get_intent_response(intent, response, cpu_usage)
+
                         elif intent["tag"] == "memory_usage_tsk":
                             response = random.choice(intent["responses"])
                             self.get_intent_response(intent, response, memory_usage)
+
                         elif intent["tag"] == "disk_space_tsk":
                             response = random.choice(intent["responses"])
                             self.get_intent_response(intent, response, disk_space)
-                        elif intent["tag"] == "opinion":
-                            response = opinion(user_input)
-                            self.get_intent_response(intent, response)
+
                         elif intent["tag"] == "time_tsk":
                             response = random.choice(intent["responses"]).replace(
                                 "{time}", self.get_time()
---
+++ functions/math/three_math_sim.py
@@ -0,0 +1,77 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sympy as sp
+import matplotlib
+
+
+matplotlib.use("TkAgg")
+
+
+def plot_custom_function(user_function_str):
+    x = sp.symbols("x")
+    y = sp.symbols("y")
+
+    user_function_expr = sp.sympify(user_function_str)
+    user_function = sp.lambdify((x, y), user_function_expr, "numpy")
+
+    x_values = np.linspace(-10, 10, 400)
+    y_values = np.linspace(-10, 10, 400)
+
+    y_custom = user_function(x_values, y_values)
+
+    fig = plt.figure(figsize=(16, 7))  # Adjust the figure size here
+
+    # Adjust the width of the 2D subplot here
+    ax2d = fig.add_subplot(131)
+
+    ax3d = fig.add_subplot(122, projection="3d")
+
+    fig.patch.set_facecolor("black")
+
+    ax2d.plot(x_values, y_custom, color="white", linewidth=2)
+    ax2d.set_xlabel("x")
+    ax2d.set_ylabel("f(x)")
+    ax2d.set_title("2D Plot of Function", color="white")
+    ax2d.set_facecolor("black")
+
+    ax2d.xaxis.label.set_color("white")
+    ax2d.yaxis.label.set_color("white")
+    ax2d.tick_params(axis="x", colors="white")
+    ax2d.tick_params(axis="y", colors="white")
+
+    X, Y = np.meshgrid(x_values, x_values)
+    Z = user_function(X, Y)
+
+    surf = ax3d.plot_surface(
+        X, Y, Z, cmap="viridis", rstride=10, cstride=10, antialiased=True
+    )
+    ax3d.set_xlabel("X")
+    ax3d.set_ylabel("Y")
+    ax3d.set_zlabel("Z")
+    ax3d.set_title("3D Plot of Function", color="white")
+
+    ax3d.set_facecolor("black")
+    ax3d.xaxis.pane.fill = False
+    ax3d.yaxis.pane.fill = False
+    ax3d.zaxis.pane.fill = False
+    ax3d.xaxis._axinfo["grid"]["color"] = "white"
+    ax3d.yaxis._axinfo["grid"]["color"] = "white"
+    ax3d.zaxis._axinfo["grid"]["color"] = "white"
+    ax3d.xaxis.label.set_color("white")
+    ax3d.yaxis.label.set_color("white")
+    ax3d.zaxis.label.set_color("white")
+    ax3d.tick_params(axis="x", colors="white")
+    ax3d.tick_params(axis="y", colors="white")
+    ax3d.tick_params(axis="z", colors="white")
+    ax3d.view_init(elev=30, azim=45)
+
+    plt.tight_layout()
+    plt.show()
+
+
+def create_simlulation_function(user_input):
+    cleaned_function_description = user_input
+    if cleaned_function_description:
+        return plot_custom_function(cleaned_function_description)
+    else:
+        pass
