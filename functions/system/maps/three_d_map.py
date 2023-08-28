c61aa18fceb3ec3ced66249f60b09e6a6298126c
Alan M. Turing <1912AlanTuring1954@gmail.com>
2023-08-27 18:25:43 -0700

    commit

--- functions/system/maps/three_d_map.py
+++ functions/system/maps/three_d_map.py
@@ -2,8 +2,8 @@ import pydeck as pdk
 import webbrowser
 
 
-def create_three_d_map(longitude, latitude):
-    # Define your data with custom coordinates and elevation for Moscow, Russia
+def create_three_d_map(longitude, latitude, output_file="3d_map.html"):
+    # Define your data with custom coordinates and elevation
     data = [{"lon": longitude, "lat": latitude, "elevation": 200}]
 
     # Define the 3D scatterplot layer for the pinpoint location
@@ -11,10 +11,10 @@ def create_three_d_map(longitude, latitude):
         "ScatterplotLayer",
         data,
         get_position="[lon, lat, elevation]",
-        get_radius=2000,  # Increase the marker size
-        get_fill_color=[0, 255, 255, 150],  # Use RGBA format for transparency (0-255)
+        get_radius=200,  # Adjust marker size for better visibility
+        get_fill_color=[0, 255, 255, 80],  # Adjust color and transparency (0-255)
         pickable=True,
-        auto_highlight=True,  # Enable hover highlighting
+        auto_highlight=True,
     )
 
     # Define the heatmap layer to indicate the location
@@ -23,35 +23,36 @@ def create_three_d_map(longitude, latitude):
         data,
         get_position="[lon, lat]",
         opacity=0.8,  # Adjust opacity as needed
-        intensity=1,  # Adjust intensity as needed
-        radius=2000,  # Adjust radius as needed
+        intensity=1,  # Reduce the intensity for a fading effect
+        radius=2000,  # Adjust radius for heatmap
+        color_range=[
+            [0, 0, 0, 0],
+            [0, 0, 255, 50],
+            [0, 0, 255, 100],
+            [0, 191, 255, 150],
+            [0, 255, 255, 200],
+            [255, 255, 0, 100],
+            [255, 0, 0, 50],
+        ],  # Customize heatmap color range for a fading effect
     )
 
     view_state = pdk.ViewState(
         longitude=longitude,
         latitude=latitude,
-        zoom=12,  # Adjust zoom level for better visibility
+        zoom=15,  # Increase zoom level for better map quality
         pitch=60,
         bearing=-30,
     )
 
-    html_file = "C:/Users/1912a/Friday/functions/system/maps/3d_map.html"
-
     # Create the 3D map using Deck.gl with both marker and heatmap layers
-    r = pdk.Deck(
+    deck = pdk.Deck(
         layers=[heatmap_layer, marker_layer],
         initial_view_state=view_state,
-        tooltip={
-            "text": "Elevation: {elevation} meters"
-        },  # Add a tooltip for elevation
+        tooltip={"text": "Elevation: {elevation} meters"},
     )
 
     # Save the 3D map to the specified HTML file
-    r.to_html(html_file)
+    deck.to_html(output_file)
 
     # Open the HTML file in a web browser
-    webbrowser.open(html_file)
-
-
-# Example usage:
-create_three_d_map(longitude=37.6176, latitude=55.7558)  # Moscow coordinates
+    webbrowser.open(output_file)
