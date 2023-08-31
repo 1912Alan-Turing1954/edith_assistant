bc1eceadc85b793b3f554b4b3a2cec3d871da124
Alan M. Turing <1912AlanTuring1954@gmail.com>
2023-08-30 21:11:03 -0700

    commit

--- functions/physics/physics_sim.py
+++ functions/physics/physics_sim.py
@@ -1,90 +1,74 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import pycuda.driver as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# Constants
-g = 9.81  # Acceleration due to gravity (m/s^2)
-air_density = 1.225  # Air density (kg/m^3)
-drag_coefficient = 0.47  # Drag coefficient for a sphere (dimensionless)
-object_radius = 0.1  # Radius of the objects (meters)
+# Define constants
+a0 = 1.0  # Bohr radius (for hydrogen-like atom)
+n = 1  # Principal quantum number
+l = 0  # Azimuthal quantum number
 
+# Create a 3D grid
+size = 200
+x = np.linspace(-10, 10, size)
+y = np.linspace(-10, 10, size)
+z = np.linspace(-10, 10, size)
+x, y, z = np.meshgrid(x, y, z)
 
-# Function to calculate drag force
-def calculate_drag_force(velocity):
-    # Calculate the magnitude of velocity
-    speed = np.linalg.norm(velocity)
-    # Calculate the drag force
-    drag_force_magnitude = (
-        0.5 * air_density * speed**2 * drag_coefficient * np.pi * object_radius**2
-    )
-    # Calculate the drag force vector
-    drag_force = -drag_force_magnitude * (velocity / speed)
-    return drag_force
-
-
-# Function to simulate physics for multiple objects
-def simulate_physics(objects, total_time, time_step=0.01):
-    # Create arrays to store time, position, and velocity values for each object
-    time_values = np.arange(0, total_time, time_step)
-    position_values = [[] for _ in objects]
-    velocity_values = [[] for _ in objects]
-
-    # Simulate the motion for each object
-    for t in time_values:
-        for i, obj in enumerate(objects):
-            position_values[i].append(obj.position.copy())
-            velocity_values[i].append(obj.velocity.copy())
-
-            # Calculate gravitational force
-            gravitational_force = np.array([0, 0, -obj.mass * g])
-
-            # Calculate drag force
-            drag_force = calculate_drag_force(obj.velocity)
-
-            # Update position and velocity using the equations of motion
-            net_force = gravitational_force + drag_force
-            obj.position += obj.velocity * time_step
-            obj.velocity += (net_force / obj.mass) * time_step
-
-            # Check for ground collision (z < 0)
-            if obj.position[2] < 0:
-                obj.position[2] = 0
-                obj.velocity[2] = 0  # Zero out vertical velocity
-
-    return time_values, position_values, velocity_values
-
-
-class PhysicsObject:
-    def __init__(self, mass, initial_position, initial_velocity):
-        self.mass = mass
-        self.position = np.array(initial_position, dtype=float)
-        self.velocity = np.array(initial_velocity, dtype=float)
-
-
-# Example usage:
-object1 = PhysicsObject(
-    mass=1.0, initial_position=[0.0, 0.0, 100.0], initial_velocity=[10.0, 0.0, 20.0]
-)
-object2 = PhysicsObject(
-    mass=0.5, initial_position=[0.0, 0.0, 100.0], initial_velocity=[15.0, 0.0, 25.0]
+# Define the CUDA kernel for electron density calculation
+mod = SourceModule(
+    """
+    __global__ void electron_density(float *x, float *y, float *z, float *density, float a0, int n, int l) {
+        int idx = threadIdx.x + blockDim.x * blockIdx.x;
+        int idy = threadIdx.y + blockDim.y * blockIdx.y;
+        int idz = threadIdx.z + blockDim.z * blockIdx.z;
+        int index = idx + idy * gridDim.x * blockDim.x + idz * gridDim.y * blockDim.y * blockDim.z;
+        
+        float r = sqrt(x[index] * x[index] + y[index] * y[index] + z[index] * z[index]);
+        
+        density[index] = pow((2 / (n * a0)) * (r / a0), l) * exp(-r / (n * a0)) / pow(n * a0, 2);
+    }
+"""
 )
 
-total_time = 10.0  # Total simulation time (seconds)
+electron_density_kernel = mod.get_function("electron_density")
 
-time, position, velocity = simulate_physics([object1, object2], total_time)
+# Allocate GPU memory
+density_gpu = np.zeros_like(x).astype(np.float32)
+x_gpu = cuda.to_device(x.astype(np.float32))
+y_gpu = cuda.to_device(y.astype(np.float32))
+z_gpu = cuda.to_device(z.astype(np.float32))
+density_gpu = cuda.to_device(density_gpu)
 
-# Create a 3D plot of the trajectory
-fig = plt.figure(figsize=(17, 8))
+# Calculate electron density on GPU
+block_size = (8, 8, 8)
+grid_size = (size // block_size[0], size // block_size[1], size // block_size[2])
+
+electron_density_kernel(
+    x_gpu,
+    y_gpu,
+    z_gpu,
+    density_gpu,
+    np.float32(a0),
+    np.int32(n),
+    np.int32(l),
+    block=block_size,
+    grid=grid_size,
+)
+
+# Copy results back to CPU
+cuda.memcpy_dtoh(density_gpu, density_gpu)
+
+# Create a 3D plot of electron density
+fig = plt.figure(figsize=(10, 10))
 ax = fig.add_subplot(111, projection="3d")
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+ax.set_zlabel("Z")
 
-for i, obj in enumerate([object1, object2]):
-    positions = np.array(position[i])
-    ax.plot(positions[:, 0], positions[:, 1], positions[:, 2], label=f"Object {i + 1}")
+ax.scatter(x, y, z, c=density_gpu, cmap="viridis", s=1)
 
-ax.set_xlabel("X (m)")
-ax.set_ylabel("Y (m)")
-ax.set_zlabel("Z (m)")
-ax.set_title("3D Trajectory of Objects")
-ax.legend()
-
+plt.title(f"Electron Density (n={n}, l={l})")
 plt.show()
